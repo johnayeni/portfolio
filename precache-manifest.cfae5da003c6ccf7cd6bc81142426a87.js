3336aa20af8a93f4d9a3737ecea8881d52f5c03f
John Ayeni <johnayeni10.aj@gmail.com>
2019-08-21 11:40:01 +0100

    Updates

---
+++ precache-manifest.cfae5da003c6ccf7cd6bc81142426a87.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b09f20440f5a21527228cbd6af8be1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e12b94bbde849a49d64f",
+    "url": "/portfolio/static/css/main.5e6f379b.chunk.css"
+  },
+  {
+    "revision": "325652bf8b4da72c5201",
+    "url": "/portfolio/static/js/2.0e451450.chunk.js"
+  },
+  {
+    "revision": "e12b94bbde849a49d64f",
+    "url": "/portfolio/static/js/main.c515f33b.chunk.js"
+  },
+  {
+    "revision": "e9da67009be6623f89a4",
+    "url": "/portfolio/static/js/runtime~main.8277f5d5.js"
+  },
+  {
+    "revision": "80a8300dc56988e9d26dc99b52ee518e",
+    "url": "/portfolio/static/media/Dosis-Bold.80a8300d.ttf"
+  },
+  {
+    "revision": "3a468f5e23070c2b94cdf60d111a6243",
+    "url": "/portfolio/static/media/Dosis-Medium.3a468f5e.ttf"
+  },
+  {
+    "revision": "f9ebd2ae6c1f2e94023a96dd1a3d12c4",
+    "url": "/portfolio/static/media/Dosis-SemiBold.f9ebd2ae.ttf"
+  },
+  {
+    "revision": "b3ff167f52b81d67a770a96c819d7156",
+    "url": "/portfolio/static/media/codepen.b3ff167f.svg"
+  },
+  {
+    "revision": "031aabdb5450cdfc6026c9a2adc96882",
+    "url": "/portfolio/static/media/dribbble.031aabdb.svg"
+  },
+  {
+    "revision": "546b9c5dbd79ddd446edc70ea6e03970",
+    "url": "/portfolio/static/media/github.546b9c5d.svg"
+  },
+  {
+    "revision": "dd1e10b6d416425361ad3274775b03d5",
+    "url": "/portfolio/static/media/instagram.dd1e10b6.svg"
+  },
+  {
+    "revision": "5cac85c6c4aa188f5c4a2d977f8959d1",
+    "url": "/portfolio/static/media/linkedin.5cac85c6.svg"
+  },
+  {
+    "revision": "dd4c6dfa1ffe7979d116541969a1e502",
+    "url": "/portfolio/static/media/logo.dd4c6dfa.svg"
+  },
+  {
+    "revision": "7c428bedb1b61f44f128ade1c736f795",
+    "url": "/portfolio/static/media/twitter.7c428bed.svg"
+  }
+]);
