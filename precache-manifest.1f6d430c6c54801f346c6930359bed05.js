6db661f0b70ed05eddb9eeb3cc93bf37b3e9e70a
John Ayeni <johnayeni10.aj@gmail.com>
2019-08-26 03:27:30 +0100

    Updates

---
+++ precache-manifest.1f6d430c6c54801f346c6930359bed05.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd7ad3735a35a183518f6a01c47ec24",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5eb803a4651ec5da8d8c",
+    "url": "/portfolio/static/css/main.68125eb9.chunk.css"
+  },
+  {
+    "revision": "325652bf8b4da72c5201",
+    "url": "/portfolio/static/js/2.0e451450.chunk.js"
+  },
+  {
+    "revision": "5eb803a4651ec5da8d8c",
+    "url": "/portfolio/static/js/main.80609da5.chunk.js"
+  },
+  {
+    "revision": "e9da67009be6623f89a4",
+    "url": "/portfolio/static/js/runtime~main.8277f5d5.js"
+  },
+  {
+    "revision": "80a8300dc56988e9d26dc99b52ee518e",
+    "url": "/portfolio/static/media/Dosis-Bold.80a8300d.ttf"
+  },
+  {
+    "revision": "3a468f5e23070c2b94cdf60d111a6243",
+    "url": "/portfolio/static/media/Dosis-Medium.3a468f5e.ttf"
+  },
+  {
+    "revision": "f9ebd2ae6c1f2e94023a96dd1a3d12c4",
+    "url": "/portfolio/static/media/Dosis-SemiBold.f9ebd2ae.ttf"
+  },
+  {
+    "revision": "076a9dae3a17b11a73e7bbb0a68ea32a",
+    "url": "/portfolio/static/media/codepen.076a9dae.svg"
+  },
+  {
+    "revision": "ea0b0c801934136b51f8a53b9c754229",
+    "url": "/portfolio/static/media/dribbble.ea0b0c80.svg"
+  },
+  {
+    "revision": "b56cd90a43b714e3b3d30786a5a82cda",
+    "url": "/portfolio/static/media/github.b56cd90a.svg"
+  },
+  {
+    "revision": "04ecb7f6a0b22d5d844cf407effda90e",
+    "url": "/portfolio/static/media/instagram.04ecb7f6.svg"
+  },
+  {
+    "revision": "0c13915484c802431f9e42064a23a05e",
+    "url": "/portfolio/static/media/linkedin.0c139154.svg"
+  },
+  {
+    "revision": "b2613b20db888a8d4f98b42f288eada6",
+    "url": "/portfolio/static/media/logo.b2613b20.svg"
+  },
+  {
+    "revision": "a4f037131784422f1c6ab5e806b5e915",
+    "url": "/portfolio/static/media/soooooooosick.a4f03713.ttf"
+  },
+  {
+    "revision": "d797c149c164b598009dd23f93d9128e",
+    "url": "/portfolio/static/media/twitter.d797c149.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d22dddcd00c241d7c177d2e6770adbda.js"
+  "/portfolio/precache-manifest.1f6d430c6c54801f346c6930359bed05.js"
 );
 
 self.addEventListener('message', (event) => {
